425a4d169cf19baad6d7338414d02fd98795bb58
AlexeySudakovJD34 <sudakovaleksey@mail.ru>
2023-03-14 15:23:19 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,15 @@
 package org.example;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PhoneBook {
+
+    public Map<String, String> contactsMap = new HashMap<>();
+
+    public int addContact(String name, String number) {
+        if (!contactsMap.containsKey(name)) {
+            contactsMap.put(name, number);
+        }
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,40 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PhoneBookTest {
+    PhoneBook sut;
+    @BeforeEach
+    public void startedEach() {
+        System.out.println("test started");
+        sut = new PhoneBook();
+    }
+    @BeforeAll
+    public static void startedAll() {
+        System.out.println("tests started");
+    }
+    @AfterEach
+    public void finishedEach() {
+        System.out.println("test completed");
+        sut = null;
+    }
+    @AfterAll
+    public static void finishedAll() {
+        System.out.println("tests completed");
+    }
+
+    @Test
+    public void testAddContact() {
+        //arrange
+        String expectedNumber = "+79999999999";
+        int expectedEntries = 2;
+        //act
+        int resultEntries1 = sut.addContact("Nikolay", "+79999999999");
+        int resultEntries2 = sut.addContact("Alisa", "+79998888888");
+        String resultNumber = sut.contactsMap.get("Nikolay");
+        //assert
+        assertEquals(expectedNumber, resultNumber);
+        assertEquals(expectedEntries, resultEntries2);
+    }
+
 }
